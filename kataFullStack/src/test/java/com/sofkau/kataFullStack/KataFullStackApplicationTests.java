c849797f7521bb9bf120b83616671defbce5491d
juan9507 <juandavidnaranjo75@gmail.com>
2022-06-15 09:08:33 -0500

    creacion del proyecto para el reto

---
+++ kataFullStack/src/test/java/com/sofkau/kataFullStack/KataFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkau.kataFullStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
